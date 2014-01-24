83bb9d3efa5c5bbdd2c2b8bb5ebbf5a6e4f1c5bf
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-01-24 13:38:28 +0100

    Include remote tests

--- test_pengines.pl
+++ test_pengines.pl
@@ -5,9 +5,16 @@
 :- use_module(library(lists)).
 :- use_module(library(debug)).
 :- use_module(library(pengines)).
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_files)).
+
+/** <module> Test suite for pengines
+*/
 
 test_pengines :-
-    run_tests([ local_pengines
+    run_tests([ local_pengines,
+		remote_pengines
 	      ]).
 
 
@@ -44,14 +51,47 @@ test(two, Sorted = [a,b,c,d,e,f]) :-
 
 :- end_tests(local_pengines).
 
-:- begin_tests(remote_pengines).
+:- begin_tests(remote_pengines,
+	       [ setup(start_pengine_server(_Port)),
+		 cleanup(stop_pengine_server)
+	       ]).
 
 test(simple, Results = [a,b,c]) :-
+    pengine_server(Server),
     pengine_create(
-	[ server('http://localhost:4040'),
+	[ server(Server),
 	  src_text("p(a). p(b). p(c).")
 	]),
     collect(X, p(X), Results, []),
+    assertion(no_more_pengines).
+test(paging, Results = [a,b,c]) :-
+    pengine_server(Server),
+    pengine_create(
+	[ server(Server),
+	  src_text("p(a). p(b). p(c).")
+	]),
+    collect(X, p(X), Results, [paging(2)]),
+    assertion(no_more_pengines).
+test(stop, Results = [a,b]) :-
+    pengine_server(Server),
+    pengine_create(
+	[ server(Server),
+	  src_text("p(a). p(b). p(c).")
+	]),
+    collect(X, p(X), Results, [stop_after(2)]),
+    assertion(no_more_pengines).
+test(two, Sorted = [a,b,c,d,e,f]) :-
+    pengine_server(Server),
+    pengine_create(
+	[ server(Server),
+	  src_text("p(a). p(b). p(c).")
+	]),
+    pengine_create(
+	[ server(Server),
+	  src_text("p(d). p(e). p(f).")
+	]),
+    collect(X, p(X), Results, []),
+    msort(Results, Sorted),
     assertion(no_more_pengines).
 
 :- end_tests(remote_pengines).
@@ -98,3 +138,28 @@ collect_handler(_, _, _, failure(Id)) :-
 
 no_more_pengines :-
     \+ pengine:current_pengine(_,_,_,_).
+
+
+		 /*******************************
+		 *	    HTTP SERVER		*
+		 *******************************/
+
+:- http_handler(/, http_reply_from_files(web, []), [prefix]).
+
+:- dynamic
+	pengine_server_port/1.
+
+pengine_server(URL) :-
+	start_pengine_server(Port),
+	format(atom(URL), 'http://localhost:~d', [Port]).
+
+start_pengine_server(Port) :-
+	pengine_server_port(Port), !.
+start_pengine_server(Port) :-
+	http_server(http_dispatch, [port(Port)]),
+	asserta(pengine_server_port(Port)).
+
+stop_pengine_server :-
+	retract(pengine_server_port(Port)), !,
+	http_stop_server(Port, []).
+stop_pengine_server.
