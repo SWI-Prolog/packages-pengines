89aa72b9be3ecb5f0bbf51b6e4b0df3dc8371cd9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-05-11 15:00:21 +0200

    TEST: Test for dangling pengines for RPC tests

--- test_pengines.pl
+++ test_pengines.pl
@@ -171,21 +171,25 @@ test(two, Sorted = [a,b,c,d,e,f]) :-
     collect(X, p(X), Results, []),
     msort(Results, Sorted),
     assertion(no_more_pengines).
-test(rpc_all, all(X == [1,2,3])) :-
+test(rpc_all, Xs == [1,2,3]) :-
     pengine_server(Server),
-    pengine_rpc(Server,
-		member(X, [1,2,3]),
-		[]).
+    findall(X, pengine_rpc(Server,
+			   member(X, [1,2,3]),
+			   []),
+	    Xs),
+    assertion(no_more_pengines).
 test(rpc_first, X == 1) :-
     pengine_server(Server),
     pengine_rpc(Server,
 		member(X, [1,2,3]),
-		[]), !.
-test(rpc_fail, fail) :-
+		[]), !,
+    assertion(no_more_pengines).
+test(rpc_fail, true) :-
     pengine_server(Server),
-    pengine_rpc(Server,
-		fail,
-		[]), !.
+    \+ pengine_rpc(Server,
+		   fail,
+		   []),
+    assertion(no_more_pengines).
 test(pengine_and_rpc, Results = [a,b,c]) :-
     pengine_server(Server),
     pengine_create(
