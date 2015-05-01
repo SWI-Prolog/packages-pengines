061f2ac6f7cca1670aa5f22546bc4fad1ac2fffe
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-05-01 15:17:43 +0200

    FIXED: Pengine examples.

--- examples/server.pl
+++ examples/server.pl
@@ -6,6 +6,7 @@
 :- use_module(library(http/http_server_files)).
 :- use_module(library(http/http_files)).
 :- use_module(library(pengines)).
+:- use_module(pengine_sandbox:library(pengines)).
 
 :- http_handler(/, http_reply_from_files(web, []), [prefix]).
 
